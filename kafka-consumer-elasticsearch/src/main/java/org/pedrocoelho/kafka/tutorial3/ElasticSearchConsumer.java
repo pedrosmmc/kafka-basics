d11a0a1c5cb62a436695e6431139f7a1b9a7171f
pedrosmmc <pedrosmmc@gmail.com>
2021-10-05 15:13:57 +0100

    kafka-consumer-elasticsearch

--- kafka-consumer-elasticsearch/src/main/java/org/pedrocoelho/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/org/pedrocoelho/kafka/tutorial3/ElasticSearchConsumer.java
@@ -1,13 +1,22 @@
 package org.pedrocoelho.kafka.tutorial3;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.replication.ReplicationResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -17,9 +26,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.UUID;
+
+import static java.util.Arrays.*;
 
 public class ElasticSearchConsumer {
-    public static RestHighLevelClient createClient() {
+    static RestHighLevelClient createClient() {
         // replace with own credentials
         String hostname = "kafka-twitter-coelho-8902158065.eu-central-1.bonsaisearch.net";
         String username = "5atlynz9y";
@@ -42,24 +57,69 @@ public class ElasticSearchConsumer {
         return client;
     }
 
+    static KafkaConsumer<String, String> createConsumer(String topic) {
+        String bootstrapServers = "localhost:9092";
+        String groupId = "kafka_twitter_elasticsearch";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+
+        KafkaConsumer consumer = new KafkaConsumer<String,String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class);
 
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{\"foo:\" \"bar\"}";
+        // Kafka consume
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
 
-        /** Add the index first on the elasticsearch:
-         *  PUT /twitter
-         */
-        IndexRequest indexRequest = new IndexRequest("twitter", "tweets").source(jsonString, XContentType.JSON);
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+            for(ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + "\nValue: " + record.value() + "\nPartition: " + record.partition()+ "\nOffset: " + record.offset());
 
-        String id = indexResponse.getId();
+                // insert data into ElasticSearch
+                /** IMPORTANTE: Add the index first on the elasticsearch:
+                 *  PUT /twitter
+                 *
+                 * 2 strategies to create ids:
+                 *  - Kafka generic id = record.topic() + record.partition() + record.offset()
+                 *  - Twitter feed specific id = extractIdFromTweet(record.value())
+                 *  - Java generated id = UUID.randomUUID().toString()
+                 */
+                IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
+                      .id(extractIdFromTweet(record.value())) // this will make our consumer idempotent
+                      .source(record.value(), XContentType.JSON);
 
-        logger.info(id);
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                // client.indexAsync(indexRequest, RequestOptions.DEFAULT, listener);
 
-        client.close(); // close client gracefully
+                logger.info(indexResponse.getId());
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+//        client.close(); // close client gracefully
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+    private static String extractIdFromTweet(String value) {
+        // using gson library
+        return jsonParser.parse(value).getAsJsonObject().get("id_str").getAsString();
     }
 }
